bbbd1958aacb23b7eeff8c4e9536d6eb23359cc8
mingdunlan <xianluofanchen0.163.com>
2018-08-26 13:44:03 +0800

    新增数据库帮助类，以及各表对应的实体类。

--- app/src/main/java/com/tcmsoso/coolwether/activity/MainActivity.java
+++ app/src/main/java/com/tcmsoso/coolwether/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.tcmsoso.coolwether;
+package com.tcmsoso.coolwether.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tcmsoso.coolwether.R;
 
 public class MainActivity extends AppCompatActivity {
 
